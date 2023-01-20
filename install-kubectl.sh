c8094370f27aeaf2325055821bfd7b648d2f254c
Evan Rosson <git@erosson.org>
2023-01-20 04:22:57 +0000

    try auto-installing gitpod kubectl

--- install-kubectl.sh
+++ install-kubectl.sh
@@ -10,4 +10,11 @@ chmod +x kubectl
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 kubectl version --client
 
+# for the vscode extension on gitpod
+#    "vscode-kubernetes.kubectl-path.linux": "/home/gitpod/.local/state/vs-kubernetes/tools/kubectl/kubectl",
+#    "vscode-kubernetes.helm-path.linux": "/home/gitpod/.local/state/vs-kubernetes/tools/helm/linux-amd64/helm",
+#    "vscode-kubernetes.minikube-path.linux": "/home/gitpod/.local/state/vs-kubernetes/tools/minikube/linux-amd64/minikube"
+mkdir -p $HOME/.local/state/vs-kubernetes/tools/kubectl
+cp kubectl $HOME/.local/state/vs-kubernetes/tools/kubectl/kubectl
+
 rm -f kubectl kubectl.sha256 $HOME/.local/bin/kubectl
