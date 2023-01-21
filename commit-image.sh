2c25198fd9cce7d0cf07ceaafe10d75cb4aac588
Evan Rosson <git@erosson.org>
2023-01-21 07:39:56 +0000

    set user, email

--- commit-image.sh
+++ commit-image.sh
@@ -5,4 +5,7 @@ PACKAGE=$1;shift
 TAG=$1;shift
 
 git add packages
+
+git config --global user.name "erosson's kubernetes deploybot"
+git config --global user.email "erosson@users.noreply.github.com"
 git commit -m "[auto] bump \`$PACKAGE\` images to \`$TAG\`"
