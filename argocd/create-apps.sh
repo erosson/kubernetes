45e03b383c3ad674d526f6c72628d05370be2690
Evan Rosson <git@erosson.org>
2023-01-21 04:16:23 +0000

    refactor config, argocd file organization

---
+++ argocd/create-apps.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+set -eu
+
+create()
+{
+    argocd app create --port-forward-namespace argocd \
+        --sync-policy automatic \
+        --revision HEAD \
+        --dest-server https://kubernetes.default.svc \
+        --dest-namespace default \
+        --upsert \
+        "$@"
+}
+
+create test-letsencrypt-staging.erosson.org \
+    --repo https://github.com/erosson/kubernetes.git \
+    --path examples/test-letsencrypt-staging.erosson.org
+
+create test-letsencrypt.erosson.org \
+    --repo https://github.com/erosson/kubernetes.git \
+    --path examples/test-letsencrypt.erosson.org
+
+create test-letsencrypt-cfproxy.erosson.org \
+    --repo https://github.com/erosson/kubernetes.git \
+    --path examples/test-letsencrypt-cfproxy.erosson.org
+
+create k8s-test-hello1.erosson.org \
+    --repo https://github.com/erosson/kubernetes.git \
+    --path examples/k8s-test-hello1.erosson.org
+
+create k8s-test-hello2.erosson.org \
+    --repo https://github.com/erosson/kubernetes.git \
+    --path examples/k8s-test-hello2.erosson.org
+
+create cluster-config \
+    --repo https://github.com/erosson/kubernetes.git \
+    --path cluster-config
