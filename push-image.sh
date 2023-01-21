28a53a75a99bf44ee9f400051e9436aa53d5b9a2
Evan Rosson <git@erosson.org>
2023-01-21 07:49:05 +0000

    back to push-image; remove header

--- push-image.sh
+++ push-image.sh
@@ -9,3 +9,7 @@ git add packages
 git config --global user.name "erosson's kubernetes deploybot"
 git config --global user.email "erosson@users.noreply.github.com"
 git commit -m "[auto] bump \`$PACKAGE\` images to \`$TAG\`"
+
+# https://stackoverflow.com/a/69979203/2782048
+git config --unset-all http.https://github.com/.extraheader
+git push
