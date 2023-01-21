0ae439e387db8071d388aeefc0d1e32439f20954
Evan Rosson <git@erosson.org>
2023-01-21 07:25:36 +0000

    create-apps multiplayer path

--- argocd/create-apps.sh
+++ argocd/create-apps.sh
@@ -5,6 +5,7 @@ create()
 {
     argocd app create --port-forward-namespace argocd \
         --sync-policy automatic \
+        --repo https://github.com/erosson/kubernetes.git \
         --revision HEAD \
         --dest-server https://kubernetes.default.svc \
         --dest-namespace default \
@@ -12,30 +13,11 @@ create()
         "$@"
 }
 
-create test-multiplayer.erosson.org \
-    --repo https://github.com/erosson/multiplayer.git \
-    --path k8s
+create test-multiplayer.erosson.org --path packages/erosson/multiplayer
 
-create test-letsencrypt-staging.erosson.org \
-    --repo https://github.com/erosson/kubernetes.git \
-    --path examples/test-letsencrypt-staging.erosson.org
-
-create test-letsencrypt.erosson.org \
-    --repo https://github.com/erosson/kubernetes.git \
-    --path examples/test-letsencrypt.erosson.org
-
-create test-letsencrypt-cfproxy.erosson.org \
-    --repo https://github.com/erosson/kubernetes.git \
-    --path examples/test-letsencrypt-cfproxy.erosson.org
-
-create k8s-test-hello1.erosson.org \
-    --repo https://github.com/erosson/kubernetes.git \
-    --path examples/k8s-test-hello1.erosson.org
-
-create k8s-test-hello2.erosson.org \
-    --repo https://github.com/erosson/kubernetes.git \
-    --path examples/k8s-test-hello2.erosson.org
-
-create cluster-config \
-    --repo https://github.com/erosson/kubernetes.git \
-    --path cluster-config
+create test-letsencrypt-staging.erosson.org --path examples/test-letsencrypt-staging.erosson.org
+create test-letsencrypt.erosson.org --path examples/test-letsencrypt.erosson.org
+create test-letsencrypt-cfproxy.erosson.org --path examples/test-letsencrypt-cfproxy.erosson.org
+create k8s-test-hello1.erosson.org --path examples/k8s-test-hello1.erosson.org
+create k8s-test-hello2.erosson.org --path examples/k8s-test-hello2.erosson.org
+create cluster-config --path cluster-config
